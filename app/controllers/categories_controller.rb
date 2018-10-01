b00be0a02b471c36851dd4d3d0539d288049eb8d
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-09-30 23:05:22 -0400

    add category controller to config.ru and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,7 @@
 class CategoriesController < ApplicationController
-  
+  get '/categories' do
+    @categories = Category.all
+    @recipes = Recipe.all
+    erb :'categories/index'
+  end
 end
--- config.ru
+++ config.ru
@@ -8,4 +8,5 @@ end
 use Rack::MethodOverride
 use RecipesController
 use UsersController
+use CategoriesController
 run ApplicationController
