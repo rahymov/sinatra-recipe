dfe676b2fb03b7ed71d7ea408ef3832a12dd8889
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-10-02 08:50:34 -0400

    add search functionality to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,14 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_categories
   has_many :categories, through: :recipe_categories
   validates_presence_of :title, :description, :ingredient, :directions
+  # include DataMapper::Resource
+  #
+  # property :id, Serial
+
+  # mount_uploader :source, ImageUploader
+
+  def self.search(search)
+    where("name like ?", "%#{search}%")
+  end
+  
 end
